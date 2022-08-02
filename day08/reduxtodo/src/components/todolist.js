f8496453e079d23c8846ee0606f276564c309c88
JM <maxsdf@naver.com>
2022-08-02 15:51:33 +0900

    "수정"

--- day08/reduxtodo/src/components/todolist.js
+++ day08/reduxtodo/src/components/todolist.js
@@ -1,17 +1,17 @@
+import { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { DELETE_TODO } from "../reducer/todo";
 const TodoList = ({ state }) => {
   const dispatch = useDispatch();
   // const state = useSelector((state) => state.todo);
-  const onRemoveClick = (id) => {
+  const onRemoveClick = useCallback(() => {
     console.log(state);
     dispatch({
       type: DELETE_TODO,
-      data: {
-        id: id,
-      },
+      id: state.id,
     });
-  };
+  }, []);
+
   // 실행문이 여러개일떄는 이렇게 만들어서 넣어준다
 
   /* 실행문으로 실행할떄는 매개변수가 있을떄 실행문으로 사용한다 */
@@ -19,7 +19,7 @@ const TodoList = ({ state }) => {
   return (
     <div>
       {state.id}.{state.todo}
-      <button onClick={() => onRemoveClick(state.id)}>완료</button>
+      <button onClick={onRemoveClick}>완료</button>
     </div>
   );
 };
